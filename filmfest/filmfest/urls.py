2d447baf0399db2e3bb44baa5457374dafd9d239
Stas Rudakou <stas@garage22.net>
2013-10-17 10:00:10 +0300

    Get rid of cmsplugin-news

--- filmfest/filmfest/urls.py
+++ filmfest/filmfest/urls.py
@@ -22,7 +22,6 @@ urlpatterns = i18n_patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^news/', include('cmsplugin_news.urls')),
 
     url(r'^2012/', include(cpm2012_urls)),
     url(r'^2013/', include(cpm2013_urls)),
