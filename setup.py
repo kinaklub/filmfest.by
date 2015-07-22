2c0e5d32d6e494702ae6aa909c0f4c7fb391fa9c
Stas Rudakou <stas@garage22.net>
2015-07-22 08:53:08 +0300

    Start using pbr in setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,7 @@
 #!/usr/bin/env python
-import os.path
-
-from setuptools import setup, find_packages
-
-
-with open(os.path.join(os.path.dirname(__file__), 'requirements')) as f:
-    install_requires = [line.strip() for line in f.readlines() if line.strip()]
+from setuptools import setup
 
 setup(
-    name='filmfest',
-    version='2.6.1',
-    description='filmfest.by, a Django-based project',
-    author='Stas Rudakou',
-    author_email='stas@garage22.net',
-    url='https://github.com/kinaklub/filmfest.by',
-    license='WTFPL',
-
-    platforms=['any'],
-    install_requires=install_requires,
-    zip_safe=False,
-    include_package_data=True,
-
-    packages=find_packages(),
-    py_modules=['manage'],
-    entry_points = {
-        'console_scripts': [
-            'filmfest_manage = manage:main',
-        ],
-    },
+    setup_requires=['pbr'],
+    pbr=True,
 )
