0870ad13998a34ba92a4c6d5c03380303a8c20e9
Stas Rudakou <stas@garage22.net>
2015-10-10 09:26:04 +0300

    Add workaround for django-cms bug 3193 (https://github.com/divio/django-cms/issues/3193)

--- manage.py
+++ manage.py
@@ -2,9 +2,19 @@
 import os
 import sys
 
+
+def fix_django_cms_3193():
+    if sys.argv[1] == 'loaddata':
+        from django.db.models import signals
+        from cms.models import Page
+        from cms.signals import update_placeholders
+        signals.post_save.disconnect(update_placeholders, sender=Page)
+
+
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "filmfest.settings")
     from django.core.management import execute_from_command_line
+    fix_django_cms_3193()
     execute_from_command_line(sys.argv)
 
 
